d8fbd9577503b07175562747131703664e833930
Negatseg <tseganehnegash@gmail.com>
2023-09-15 10:20:27 -0500

    modified the java script and the html files

--- script.js
+++ script.js
@@ -4,30 +4,55 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-const lower = ['a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,,u,v,w,x,y,z,'];
-const upper = ['A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,'];
-const numbers = [1,2,3,4,5,6,7,8,9,0,];
-const symbols = ["!,@,#,$,%,^,&,*,(,),?,>,<,=,+,-,_"];
-const passwordLength= document.getElementById('length')
+const lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+const upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+const numbers = ['1','2','3','4','5','6','7','8','9','0'];
+const symbols = ['!','@','#','$','%','^','&','*','(',')','?','>','<','=','+','-'];
+const passwordLengthEl = document.getElementById('length')
+const uppercaseEl = document.getElementById("uppercase")
+const lowercaseEl = document.getElementById("lowercase")
+const numbersEl = document.getElementById("numbers")
+const symbolsEl = document.getElementById("symbols")
+const passwordOptions = []
+const password = []
 
-function generatePassword(lower,upper,numbers,symbols){
-  
-  const lowerCases =confirm(" would you like to include lower cases?")
-  const upeerCases =confirm(" would you like to include upper cases?")
-  const numeric= confirm (" would you like  to include numberic values?")
-  const chars= confirm(" would you like to include lower special characters?")
+uppercaseEl.addEventListener("change" , () => {
+  let wantOption = uppercaseEl.checked
+  if(wantOption){
+  passwordOptions.push(...upper)
+  } 
+})
+lowercaseEl.addEventListener("change" , () => {
+  let wantOption = lowercaseEl.checked
+  if(wantOption){
+    passwordOptions.push(...lower)
+  } 
+})
+numbersEl.addEventListener("change" , () => {
+  let wantOption = numbersEl.checked
+  if(wantOption){
+    passwordOptions.push(...numbers)
+  } 
+})
+symbolsEl.addEventListener("change" , () => {
+  let wantOption = symbolsEl.checked
+  if(wantOption){
+   passwordOptions.push(...symbols)
+  } 
+})
+function generatePassword(){
+  const passwordLength = parseInt(passwordLengthEl.value)
 
-  let password = "";
-  for (var i=0; i <passwordLength; i++) {
-    let randomNumber = Math.floor(Math.random()* passwordLength.length);
-      password+= lower.substring(randomNumber, randomNumber + 1);
-      password+= upper.substring(randomNumber, randomNumber + 1);
-      password+= numbers.substring(randomNumber, randomNumber + 1);
-      password+= symbols.substring(randomNumber, randomNumber + 1);
-  }
-  document.getElementById('password').value = password;
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)){
+    alert(" must be a number value greater than 8 and less than 128")
+}
 
-  console.log(password);
+for (let i = 0; i < passwordLength; i++){
+  const mixItUp = Math.floor(Math.random() * passwordOptions.length)
+  password.push(passwordOptions[mixItUp])
+}
+ 
+return password.join("")
 }
 // Write password to the #password input
 function writePassword() {
@@ -35,11 +60,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-return;"password";
 }
-
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword)
-
-
